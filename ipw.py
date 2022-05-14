76f51f91f577f7f8a59764d6410521c941570611
Henry Zhu <hyzhu@stanford.edu>
2022-05-14 15:51:06 -0700

    simulation code

--- ipw.py
+++ ipw.py
@@ -1,14 +1,178 @@
 import sys
 print(sys.path)
 import numpy as np
-with dview.sync_imports():
-    from numpy import exp, where, mean, minimum
+# with dview.sync_imports():
+#     from numpy import exp, where, mean, minimum
 from hiv import HIVTreatment as model
 
 
 import pickle
 import matplotlib.pyplot as plt
-from tqdm import tqdm
+# from tqdm import tqdm
 from scipy.stats import sem
 from scipy import stats
 
+
+### Environment Params ###
+# dt = 1
+# episode_length = 1000/dt
+perturb_rate = 0.0
+num_actions = 4
+num_states = 6
+num_patients = 30
+ins = 20
+B = 0.1
+
+## Rollout a Trajectory ##
+def encode_action(action):
+    a = np.zeros(num_actions)
+    a[action] = 1
+    return a
+
+def random_policy(state):
+    return np.random.randint(0, num_actions)
+
+def constant_hazard_threshold_policy(obs, prev_action, beta, c, B, dt):
+    """ Prev_action is binary 0-1. Returns probability of taking action 1. """
+    if prev_action==0 and (10**obs)@beta >= c:
+        return B*dt
+    elif prev_action != 0 and (10**obs)@beta < c:
+        return 1 - B*dt
+    return int(prev_action != 0)
+
+def log_linear_policy(obs, prev_action, beta, c, B, dt):
+    """ Prev_action is binary 0-1. Returns P(taking action 1) = exp(beta@obs + c) dt. 
+        Setup is in prep for logistic regression down the line. """
+    switch_probs_01 = minimum(B * dt / (1 + exp((10**obs) @ beta - c)), 1) # P(1 | 0)
+    stay_probs_11 = 1 - minimum(B * dt / (1 + exp(- ((10**obs) @ beta - c))), 1) # P(1 | 1)
+    return where(prev_action == 0, switch_probs_01, stay_probs_11)
+    
+def get_data_parallel(policy, dt=5, total_days=1000, num_patients=30):
+#     track = True
+
+    
+#     data = []
+
+#     for _ in tqdm(range(num_patients)): # parallelize this
+    def run_traj(seed):
+            import sys
+            sys.path.append('/Users/henryzhu/Research/data/RepBM')
+            from hiv_domain.hiv_simulator.hiv import HIVTreatment as model
+            import numpy as np
+            episode_length = total_days/dt
+            env = model(perturb_rate = perturb_rate, dt=dt)
+
+            np.random.seed(seed)
+
+            env.reset(perturb_params =  True, **env_params) # default to params from Liu
+            state = env.observe()   
+            # task is done after max_task_examples timesteps or when the agent enters a terminal state
+    #         ep_list = []
+            state_list = []
+            action_list = []
+            ep_reward = 0
+            prev_action = 0
+            rewards = []
+            policy_probs = []
+
+            while not env.is_done(episode_length=episode_length):
+            #     action = self.policy(state, eps)
+                state_list.append(state)
+                prob_take_action_1 = policy(state, prev_action)
+                
+                action = 3 * int(np.random.random() < prob_take_action_1)
+                action_list.append(action)
+                reward, next_state = env.perform_action(action, perturb_params=True, **env_params)
+    #             if not track: tmp.append(np.hstack([state,action,reward, next_state]))
+    #             else: 
+
+    #             ep_list.append(np.array([state, encode_action(action),reward,next_state, ins]))
+                state = next_state
+                prev_action = int(action != 0)
+            #     ep_reward += (reward*self.gamma**self.task.t)
+#                 ep_reward += reward
+                rewards.append(reward)
+                if action == 0:
+                    policy_probs.append(1 - prob_take_action_1)
+                else:
+                    policy_probs.append(prob_take_action_1)
+    #         if track:
+    #             pass
+                # print(np.unique(action_list, return_counts = True),ep_reward)
+#             ep_reward /= episode_length
+            ep_reward = np.median(rewards)
+            return {"states": np.array(state_list), "actions": np.array(action_list), "outcome": ep_reward,
+                   "policy_probs": policy_probs}
+#             data.append()
+#     with ipp.Cluster() as rc:
+        
+#         # get a view on the cluster
+#         view = rc.load_balanced_view()
+        
+#         rc[:].push(dict(
+#             log_linear_policy=log_linear_policy,
+#             beta_1=beta_1,
+#             c1=c1
+#         ))a
+        # submit the tasks
+    dview.push(dict(env_params=preset_hidden_params[ins],
+                   perturb_rate=perturb_rate,
+                   total_days=total_days,
+                   dt=dt,
+                   policy=policy))
+    asyncresult = dview.map_async(run_traj, list(range(num_patients)))
+    # wait interactively for results
+    asyncresult.wait_interactive()
+#     asyncresult.wait()
+    # retrieve actual results
+    data = asyncresult.get()
+    return data
+
+def get_data(policy, dt=5, total_days=1000, num_patients=30):
+    data = []
+    for _ in tqdm(range(num_patients)): # parallelize this
+            episode_length = total_days/dt
+            env = model(perturb_rate = perturb_rate, dt=dt)
+            env.reset(perturb_params =  True, **env_params) # default to params from Liu
+            state = env.observe()   
+            # task is done after max_task_examples timesteps or when the agent enters a terminal state
+    #         ep_list = []
+            state_list = []
+            action_list = []
+            ep_reward = 0
+            prev_action = 0
+            rewards = []
+            policy_probs = []
+
+            while not env.is_done(episode_length=episode_length):
+            #     action = self.policy(state, eps)
+                state_list.append(state)
+                prob_take_action_1 = policy(state, prev_action)
+                
+                action = 3 * int(np.random.random() < prob_take_action_1)
+                action_list.append(action)
+                reward, next_state = env.perform_action(action, perturb_params=True, **env_params)
+    #             if not track: tmp.append(np.hstack([state,action,reward, next_state]))
+    #             else: 
+
+    #             ep_list.append(np.array([state, encode_action(action),reward,next_state, ins]))
+                state = next_state
+                prev_action = int(action != 0)
+            #     ep_reward += (reward*self.gamma**self.task.t)
+#                 ep_reward += reward
+                rewards.append(reward)
+                if action == 0:
+                    policy_probs.append(1 - prob_take_action_1)
+                else:
+                    policy_probs.append(prob_take_action_1)
+            ep_reward = np.median(rewards)
+            data.append({"states": np.array(state_list), "actions": np.array(action_list), "outcome": ep_reward,
+                   "policy_probs": policy_probs})
+    return data
+
+
+beta_1, c1 = np.array([0, 0, 0, 0, 0.00002, -0.2]), -3
+loglin_pol_1 = lambda obs, prev_action: log_linear_policy(
+    obs, prev_action, beta_1, c1, B=0.1, dt=5)
+
+data_loglin_pol_1_dt_5 = get_data(loglin_pol_1, dt=5)
